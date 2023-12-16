b0f81cf84a74969b046a08c6159e1ce13d9c67b1
yaswanth <yaswanthm@presidio.com>
2023-12-16 18:22:33 +0530

    Go application to check whether web server is reachable

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Healthchecker",
+		Usage: "A tiny tool that checks whether a website is running or is down",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "Domain name to be checked",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Usage:    "port number to be checked",
+				Required: false,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			port := c.String("port")
+			if c.String("port") == "" {
+				port = "80"
+			}
+			status := Check(c.String("domain"), port)
+			fmt.Println(status)
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
